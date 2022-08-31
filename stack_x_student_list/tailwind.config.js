d9c980d0dbfc7ea629783dd4c1e7a486e4b8d6b1
Leonardo Santos <leonardojosantos@gmail.com>
2022-08-31 13:23:39 -0300

    Parte I
    Realizado configurações iniciais.

---
+++ stack_x_student_list/tailwind.config.js
@@ -0,0 +1,33 @@
+
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './public/index.html',
+    "./src/**/*.{html,js, Vue}"
+  ],
+
+  theme: {
+    extend: {},
+    
+    colors: {
+      'white--900': '#FFFFFF',
+      'white--600': '#F0F0F0',
+      'white--400': '#EDEDED',
+      'grey--300': '#C6C6D3',
+      'grey--400': '#A1A1AA',
+      'dark--300': '#3F3F46',
+      'dark--400': '#27272A',
+      'dark--500': '#3E3E41',
+      'dark--600': '#1C1C1C',
+      'dark--700': '#09090A',
+    },
+
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Merriweather', 'serif']
+    }
+  },
+
+  plugins: [],
+}
